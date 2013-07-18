0e020d1dbad2c60eb5085708fadd8f7ded9c978e
jerkey <jerkey@halo.nu>
2013-07-18 16:03:09 -0700

    comment the code jake style

--- neogame.ino
+++ neogame.ino
@@ -21,7 +21,7 @@ uint32_t c1, c2;
 int puck = strip.numPixels()/2;
 
 void setup() {
-  pinMode(B1, INPUT);
+  pinMode(B1, INPUT); // pins default to inputs anyway
   pinMode(B2, INPUT);
   pinMode(B3, INPUT);
   digitalWrite(B1, HIGH); // turn on pull-up resistors for buttons
@@ -29,41 +29,41 @@ void setup() {
   digitalWrite(B3, HIGH);
 
   Serial.begin(9600);
-  strip.begin();
+  strip.begin();  // parameters in variable "strip"
   strip.show(); // Initialize all pixels to 'off'
 
-  c1 = strip.Color(255, 0, 0);  // c1 = red?
-  c2 = strip.Color(0, 255, 0);  // c2 = green?
+  c1 = strip.Color(255, 0, 0);  // c1 = red
+  c2 = strip.Color(0, 255, 0);  // c2 = green
 }
 
 void game_step() {
   strip.setPixelColor(puck, strip.Color(255, 255, 255));  // puck, white, where it goes
 
   for (int i=puck-1; i > -1; i--) {
-    strip.setPixelColor(i, strip.getPixelColor(i-1));
+    strip.setPixelColor(i, strip.getPixelColor(i-1));  // move pixels up toward puck
   }
   for (int i=puck+1; i < strip.numPixels(); i++) {
-    strip.setPixelColor(i, strip.getPixelColor(i+1));
+    strip.setPixelColor(i, strip.getPixelColor(i+1));  // move pixels down toward puck
   }
 
-  if (strip.getPixelColor(puck-1)) {
+  if (strip.getPixelColor(puck-1)) {  // if red is just below puck, move puck up toward green
     strip.setPixelColor(puck-1, 0);
     puck++;
   }
-  if (strip.getPixelColor(puck+1)) {
+  if (strip.getPixelColor(puck+1)) { // if green is just above puck, move puck down toward red
     strip.setPixelColor(puck+1, 0);
     puck--;
   }
 } // game_step()
 
 int b1prev, b2prev;
-boolean debounce(int button, int* prev) {
+boolean debounce(int button, int* prev) {  // doesn't actually debounce
   int newval = digitalRead(button);
   int oldval = *prev;
   *prev = newval;
 
   return (newval == 0) && (newval != oldval);
-}
+} // debounce(
 
 int lockout = 0;
 int locker = 0;
@@ -73,31 +73,32 @@ void handleButtons() {
 
   if (lockout) { lockout--; }
 
-  if ( (b1 | b2) && (b1 ^ b2) ) {
+  if ( (b1 | b2) && (b1 ^ b2) ) {  // if one button is pressed
     if (lockout == 0) {
-      locker = b1 ? B1 : B2 ;
+      locker = b1 ? B1 : B2 ;  // say which button pin number locked (firsted) it
       lockout = TIMEWINDOW;       // 20 is the count value for the time window
-    } 
-    else {
-      if (locker == B1 && b2) {  
+    }
+    else {  // we're firsted, who hit it 2nd?
+      if ((locker == B1) && b2) {  // if B1 firsted it and b2 is pressed
         Serial.println("pew!");
-        strip.setPixelColor(0, c1);
+        strip.setPixelColor(0, c1);  // b1 fires a shot from 0!
         lockout = 0;
       }  
-      else if (locker == B2 && b1) {
+      else if ((locker == B2) && b1) {  // if B2 firsted it and b1 is pressed
         Serial.println("bew!");
-        strip.setPixelColor(strip.numPixels()-1, c2);
+        strip.setPixelColor(strip.numPixels()-1, c2);  // b2 fires a shot from n-1!
         lockout = 0;
       }
     }
   }
-}
+}  // handleButtons()
 
 void loop() {  
   game_step();
   handleButtons();
   strip.show();
 }
+
 
 
 
