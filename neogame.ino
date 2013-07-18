63aab499347a8865e183bf08260abe3f812fc011
jerkey <jerkey@halo.nu>
2013-07-18 16:00:13 -0700

    basic define name improvements

--- neogame.ino
+++ neogame.ino
@@ -1,6 +1,6 @@
 #include <Adafruit_NeoPixel.h>
 
-#define PIN 13
+#define STRIPPIN 13
 
 // Parameter 1 = number of pixels in strip
 // Parameter 2 = pin number (most are valid)
@@ -9,11 +9,13 @@
 //   NEO_GRB     Pixels are wired for GRB bitstream
 //   NEO_KHZ400  400 KHz bitstream (e.g. FLORA pixels)
 //   NEO_KHZ800  800 KHz bitstream (e.g. High Density LED strip)
-Adafruit_NeoPixel strip = Adafruit_NeoPixel(240, PIN, NEO_GRB + NEO_KHZ800);
+Adafruit_NeoPixel strip = Adafruit_NeoPixel(240, STRIPPIN, NEO_GRB + NEO_KHZ800);
 
 #define B1 A6
 #define B2 A8
 #define B3 A7
+
+#define TIMEWINDOW 20  // HOW MANY cycles is the time window
 
 uint32_t c1, c2;
 int puck = strip.numPixels()/2;
@@ -74,7 +76,7 @@ void handleButtons() {
   if ( (b1 | b2) && (b1 ^ b2) ) {
     if (lockout == 0) {
       locker = b1 ? B1 : B2 ;
-      lockout = 20;
+      lockout = TIMEWINDOW;       // 20 is the count value for the time window
     } 
     else {
       if (locker == B1 && b2) {  
